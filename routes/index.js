2ca2812e37658d28b0e1ce51526c2dd030c03ec7
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2022-03-06 16:57:31 -0500

    lab 2 changes
    sql, hbs, js
    post /:id not working

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HackBCA 20XX', style: "index" });
-});
-
-module.exports = router;
--- webapp2022_hackbca-main/app.js
+++ webapp2022_hackbca-main/app.js
@@ -8,20 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/events');
 
+const { auth } = require('express-openid-connect');
+const db = require("./db/db");
+
 var app = express();
-const { auth } = require('express-openid-connect');const { fdatasync } = require('fs');
-const db = require("./db/db")
-jdja
-
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: '6NqMDxZkW25jX914Y8pemvRyrmTfewmI',
-  issuerBaseURL: 'https://dev-4s47rktj.us.auth0.com'
-};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,24 +23,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('dotenv').config()
+
+const authConfig = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: process.env.AUTH_BASEURL,
+  clientID: process.env.AUTH_CLIENTID,
+  issuerBaseURL: process.env.AUTH_ISSUERBASEURL
+};
+
+app.use(auth(authConfig));
+
+app.use( async (req, res, next) => {
+  res.locals.isAuthenticated = req.oidc.isAuthenticated();
+  if (res.locals.isAuthenticated){
+    //check if admin
+    let results = await db.queryPromise("SELECT admin FROM user WHERE email = ?", [req.oidc.user.email])
+    if (results.length > 0) {
+      res.locals.isAdmin = (results[0].admin == 1)
+    } else {
+      //if no account yet, set up user row in database (account information)
+      //For now, we'll just make a quick "account" with just the email info
+      await db.queryPromise("INSERT INTO user (email) VALUES (?)", [req.oidc.user.email]);
+      res.locals.isAdmin = false;
+    }
+  }
+  next();
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
-app.use(auth(config));
-
-app.use(async  (req, res, next) => {
-  res.locals = req.oidc.isAuthenticated();
-  if (res.locals.isAuthenticated) {
-    // check if admin
-    let results = await db.queryPromise("SELECT admin FROM user WHERE email = ?", [req.oidc.user.email])
-   if (results.length > )
-   
-    res.locals.isAdmin = results[0].admin == 1
-  }
-  next();
-
-})
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -69,12 +74,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
